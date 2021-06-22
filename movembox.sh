dc979dd2edb8e1613adda174daceab3dd5c3932c
Nikolay A. Fetisov <naf@altlinux.ru>
2021-06-22 15:21:21 +0300

    Translate messages from Spanish to English

--- movembox.sh
+++ movembox.sh
@@ -1,40 +1,40 @@
 #!/bin/bash
 
-echo "Transferencia entre buzones. v.0.1"
+echo "Transfer mailboxes between mailstores. v.0.1"
 echo "RUN AS ZIMBRA USER!"
 
-echo -n "Ingrese la cuenta (correo electrónico) que desea mover: "; read ACCOUNT1
+echo -n "Enter the account (email) you want to move:"; read ACCOUNT1
 zimbraMailHost=`zmprov -l ga $ACCOUNT1 | grep zimbraMailHost`
 test=${zimbraMailHost:0:14}
 if [ "$test" != "zimbraMailHost" ]; then
-echo "¡La cuenta $ACCOUNT1 no existe!"
+echo "Account ${ACCOUNT1} does not exist!"
 exit 255;
 fi
 
 SERVER1=${zimbraMailHost:16}
-echo "Servidor de origen (ubicación actual): $SERVER1"
+echo "Origin server (current location): $SERVER1"
 
-echo -n "Por favor, introduzca un servidor destino: "; read SERVER2
-if [ $SERVER1 == $SERVER2 ]; then
-echo "¡Los servidores de origen y destino no pueden ser el mismo!"
+echo -n "Please enter a destination server:"; read SERVER2
+if [ "${SERVER1}" == "${SERVER2:?}" ]; then
+echo "The source and destination servers cannot be the same!"
 exit 255;
 fi
 
-echo -n "Ingrese un directorio temporal para la ubicación de datos: "; read TEMPDIR
-if [ ! -d $TEMPDIR ]; then
-echo "¡El directorio $TEMPDIR no existe!"
+echo -n "Enter a temporary directory for the data location: "; read TEMPDIR
+if [ ! -d "${TEMPDIR}" ]; then
+echo "The ${TEMPDIR} directory does not exist!"
 exit 255;
 fi
 
-# creando una nueva cuenta
-echo "Se está creando una cuenta temporal en el servidor $SERVER2..."
-TEMPACCOUNT="temp_$ACCOUNT1"
+# creating a new account
+echo "A temporary account is being created on the server ${SERVER2}..."
+TEMPACCOUNT="temp_${ACCOUNT1}"
 
-# contraseña y otras cosas
-echo "Copiando la configuración de la cuenta anterior..."
+# password and other things
+echo "Copying the previous account settings..."
 
-NAMA_FILE="$TEMPDIR/zcs-acc-add.zmp"
-LDIF_FILE="$TEMPDIR/zcs-acc-mod.ldif"
+NAMA_FILE="${TEMPDIR}/zcs-acc-add.zmp"
+LDIF_FILE="${TEMPDIR}/zcs-acc-mod.ldif"
 
 rm -f $NAMA_FILE
 rm -f $LDIF_FILE
@@ -62,7 +62,7 @@ sn=`/opt/zimbra/common/bin/ldapsearch -H $LDAP_MASTER_URL -w $ZIMBRA_LDAP_PASSWO
 
 
 if [ $ACC = "admin" ] || [ $ACC = "wiki" ] || [ $ACC = "galsync" ] || [ $ACC = "ham" ] || [ $ACC = "spam" ]; then
-    echo "¡No se pueden mover las cuentas del sistema!"
+    echo "System accounts cannot be moved!"
     exit 255
 else
     echo "createAccount $TEMPACCOUNT XXXccc123wQ displayName '$displayName' givenName '$givenName' sn '$sn' initials '$initials' zimbraPasswordMustChange FALSE zimbraMailHost $SERVER2" >> $NAMA_FILE
@@ -81,41 +81,41 @@ fi
 
 if [ -f $NAMA_FILE ];
 then
-    if [ -f $LDIF_FILE ];
+    if [ -f "${LDIF_FILE}" ];
         then
             zmprov < $NAMA_FILE
             ldapmodify -f $LDIF_FILE -x -H $LDAP_MASTER_URL -D cn=config -w $ZIMBRA_LDAP_PASSWORD
         else
-            echo "Error, el archivo $LDIF_FILE no existe."
+            echo "Error, the ${LDIF_FILE} file does not exist."
             exit 255
         fi
 else
-    echo "Error, el archivo $NAMA_FILE no existe."
+    echo "Error, the $NAMA_FILE file does not exist."
     exit 255
 fi
 
-echo "Cuenta temporal creada."
+echo "Temporary account created."
 
-echo "Iniciando la transferencia de datos."
+echo "Starting the data transfer."
 
-echo "Copia de seguridad de $ACCOUNT1 anterior en el servidor $SERVER1"
+echo "Backup of previous ${ACCOUNT1} on server ${SERVER1}"
 ZMBOX=/opt/zimbra/bin/zmmailbox
 DATE=`date +"%Y%m%d%H%M%S"`
-$ZMBOX -z -m $ACCOUNT getRestURL "//?fmt=tgz" > $TEMPDIR/$ACCOUNT1.$DATE.tar.gz
+$ZMBOX -z -m $ACCOUNT getRestURL "//?fmt=tgz" > "${TEMPDIR}/{$ACCOUNT1}.${DATE}.tar.gz"
 
-echo "Restaurar datos a la cuenta temporal $TEMPACCOUNT del servidor $SERVER2"
-$ZMBOX -z -m $TEMPACCOUNT postRestURL "//?fmt=tgz&resolve=reset" $TEMPDIR/$ACCOUNT1.$DATE.tar.gz
+echo "Restore data to the temporary account ${TEMPACCOUNT} on the server ${SERVER2}"
+$ZMBOX -z -m $TEMPACCOUNT postRestURL "//?fmt=tgz&resolve=reset" "${TEMPDIR}/${ACCOUNT1}.${DATE}.tar.gz"
 
-echo "Cambiar el nombre de la cuenta anterior a old_$ACCOUNT1..."
-# renombrar el viejo
-zmprov renameAccount $ACCOUNT1 old_$ACCOUNT1
+echo "Rename the old account to old_${ACCOUNT1}..."
+# rename the old
+zmprov renameAccount "${ACCOUNT1}" "old_${ACCOUNT1}"
 
-echo "Cerrar cuenta antigua old_$ACCOUNT1..."
-zmprov ma old_$ACCOUNT1  zimbraAccountStatus closed
+echo "Close old account old_${ACCOUNT1}..."
+zmprov ma "old_${ACCOUNT1}"  zimbraAccountStatus closed
 
-echo "Renombrar cuenta antigua old_ $ACCOUNT1 ..."
-# renombrar nuevo
-zmprov renameAccount $TEMPACCOUNT $ACCOUNT1
+echo "Rename temporary account to ${ACCOUNT1} ..."
+# rename new
+zmprov renameAccount "${TEMPACCOUNT}" "${ACCOUNT1}"
 
-echo "HECHO."
+echo "DONE."
 exit
